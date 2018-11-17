515ff0614a2d20f7ffcdb7b218a72292fe90a873
jem <jeremy_ng@mymail.sutd.edu.sg>
2018-11-17 21:34:57 +0800

    changes to login page

--- app/src/main/java/com/jem/fq/LoginPage.java
+++ app/src/main/java/com/jem/fq/LoginPage.java
@@ -2,12 +2,32 @@ package com.jem.fq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
+    private String[] sql = {
+            "9028267311", "1234567890", "0987654321"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        EditText storeID = findViewById(R.id.storeID);
+        storeID.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    Toast.makeText(LoginPage.this, "hi", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
